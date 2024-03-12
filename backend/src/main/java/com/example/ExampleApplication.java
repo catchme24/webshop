28a53978c7f838a3df2fc6f0af7b4c3c68e8515b
catchme24 <roslyakov119@gmail.com>
2024-03-12 16:02:51 +0400

    added sout and dependency lombok

--- backend/src/main/java/com/example/ExampleApplication.java
+++ backend/src/main/java/com/example/ExampleApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class ExampleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("НОВЫЙ КОММИТ");
 		SpringApplication.run(ExampleApplication.class, args);
 	}
+
 
 }
